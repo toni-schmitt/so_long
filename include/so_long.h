c6d96cd26e07c642e9c60e286aa1f825189c14c0
Toni Schmitt <tschmitt@student.42heilbronn.de>
2021-09-05 16:56:27 +0200

    add ability to move player

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 16:21:00 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/09/04 18:33:06 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/09/05 16:52:51 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,8 @@ typedef struct s_map
 	int			line_count;
 	t_vector	*positions[4];
 	int			pos_count[4];
+	int			movement_counter;
+	int			collected;
 }	t_map;
 
 typedef struct s_mlx
@@ -111,5 +113,6 @@ void	new_sprite(void *mlx, t_image *image);
 void	put_image(t_data *data, int image, unsigned int x, unsigned int y);
 void	init_texture_paths(t_data *data);
 void	init_positions(t_map *map);
+int		win_game(t_data *data);
 
 #endif
--- src/handle_input.c
+++ src/handle_input.c
@@ -6,7 +6,7 @@
 /*   By: tschmitt <tschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/04 14:56:42 by tschmitt          #+#    #+#             */
-/*   Updated: 2021/09/04 15:02:57 by tschmitt         ###   ########.fr       */
+/*   Updated: 2021/09/05 16:53:04 by tschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,74 @@ enum
 	right = 3
 };
 
-static int	move(t_data *data, int direction)
+static void	update_collectible(t_map *map, int x_plus, int y_plus)
 {
+	int	i;
+
+	i = 0;
+	while (i < map->pos_count[collectible])
+	{
+		if (map->positions[collectible][i].x == \
+			map->positions[player]->x + x_plus &&
+			map->positions[collectible][i].y == \
+			map->positions[player]->y + y_plus)
+		{
+			map->positions[collectible][i].x = FALSE;
+			map->positions[collectible][i].y = FALSE;
+			map->collected++;
+		}
+		i++;
+	}
+}
+
+static int	is_movable_space(t_data *data, int x_plus, int y_plus)
+{
+	int	j;
+
+	if (data->map.positions[player]->x + x_plus > data->win_width || \
+		data->map.positions[player]->y + y_plus > data->win_height || \
+		data->map.positions[player]->x + x_plus < 0 || \
+		data->map.positions[player]->y + y_plus < 0)
+		return (FALSE);
+	j = 0;
+	while (j < data->map.pos_count[wall])
+	{
+		if (data->map.positions[player]->x + x_plus == \
+			data->map.positions[wall][j].x &&
+			data->map.positions[player]->y + y_plus == \
+			data->map.positions[wall][j].y)
+			return (FALSE);
+		j++;
+	}
+	return (TRUE);
+}
+
+static int	is_moving_to_exit(t_data *data, int x_plus, int y_plus)
+{
+	if (data->map.positions[player]->x + x_plus == \
+		data->map.positions[map_exit]->x && \
+		data->map.positions[player]->y + y_plus == \
+		data->map.positions[map_exit]->y)
+		return (TRUE);
+	return (FALSE);
+}
+
+static int	move(t_data *data, int x_plus, int y_plus)
+{
+	x_plus *= TEXTURE_WIDTH;
+	y_plus *= TEXTURE_HEIGHT;
+	if (!is_movable_space(data, x_plus, y_plus))
+		return (-1);
+	if (is_moving_to_exit(data, x_plus, y_plus))
+		return (win_game(data));
+	update_collectible(&data->map, x_plus, y_plus);
+	put_image(data, grass_flower, data->map.positions[player]->x, \
+	data->map.positions[player]->y);
+	put_image(data, statue, data->map.positions[player]->x + x_plus, \
+	data->map.positions[player]->y + y_plus);
+	ft_printf("%i\n", ++data->map.movement_counter);
+	data->map.positions[player]->x += x_plus;
+	data->map.positions[player]->y += y_plus;
 	return (1);
 }
 
@@ -30,12 +96,12 @@ int	handle_keys(int key, t_data *data)
 	if (key == KEY_ESCAPE)
 		return (close_window(data));
 	if (key == KEY_W)
-		return (move(data, up));
+		return (move(data, 0, -1));
 	if (key == KEY_S)
-		return (move(data, down));
+		return (move(data, 0, +1));
 	if (key == KEY_A)
-		return (move(data, left));
+		return (move(data, -1, 0));
 	if (key == KEY_D)
-		return (move(data, right));
+		return (move(data, +1, 0));
 	return (-1);
 }
